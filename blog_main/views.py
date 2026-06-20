7377817d6145b0049818e620ad3a9b9de7bfab91
Tusharkanta Behera <tusharkantabehera85@gmail.com>
2026-06-20 22:22:17 +0530

    Update the Post page.

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured = True)
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+    }
+    return render(request, 'home.html', context)

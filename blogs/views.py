6849f06a12ddc070e7cbe1aded748a67c184c1a2
Tusharkanta Behera <tusharkantabehera85@gmail.com>
2026-06-23 19:54:55 +0530

    Add the category part.

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+
+from .models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id):
+
+    posts = Blog.objects.filter(status='published', category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+
+    #category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
